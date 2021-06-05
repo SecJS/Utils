6f51d4fcf313a6d52988be9bbedf4c6798948bfd
João Lenon <lenonsec7@gmail.com>
2021-06-05 12:57:51 -0300

    feat: Add fullPath boolean in getFolder

--- src/Functions/getFolders.ts
+++ src/Functions/getFolders.ts
@@ -1,5 +1,5 @@
-import { resolve } from 'path'
 import { promises } from 'fs'
+import { resolve, basename } from 'path'
 
 export interface DirectoryContract {
   path: string
@@ -13,17 +13,19 @@ export interface DirectoryContract {
  * @param dir The directory
  * @param withFiles If need to get files inside folders by default false
  * @param buffer Return the buffer of the file by default false
+ * @param fullPath Return the full path of folder or archive by default false
  * @return The directory root with sub folders and files when withFiles true
  */
 export async function getFolders(
   dir: string,
   withFiles = false,
   buffer = false,
+  fullPath = false,
 ): Promise<DirectoryContract> {
   const dirents = await promises.readdir(dir, { withFileTypes: true })
 
   const directory = {
-    path: resolve(dir),
+    path: fullPath ? resolve(dir) : basename(resolve(dir)),
     files: [],
     folders: [],
   }
@@ -32,12 +34,18 @@ export async function getFolders(
     const res = resolve(dir, dirent.name)
 
     if (dirent.isDirectory()) {
-      directory.folders.push(await getFolders(res, withFiles, buffer))
+      directory.folders.push(await getFolders(res, withFiles, buffer, fullPath))
 
       continue
     }
 
     if (dirent.isFile() && withFiles) {
+      if (!fullPath && !buffer) {
+        directory.files.push(basename(res))
+
+        continue
+      }
+
       directory.files.push(buffer ? Buffer.from(res) : res)
     }
   }
