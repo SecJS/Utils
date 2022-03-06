d96e24d5e6d1a92fd238be418f352a5d3719604f
João Lenon <lenonSec7@gmail.com>
2022-03-05 22:08:07 -0300

    feat: add config class to project

---
+++ tests/stubs/test-error.ts
@@ -0,0 +1,12 @@
+/**
+ * @secjs/utils
+ *
+ * (c) João Lenon <lenon@secjs.com.br>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export const config = {
+  error: true,
+}
