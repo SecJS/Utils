d5f3d70e36479f9c719d676ed226d467f176dd98
João Lenon <lenonSec7@gmail.com>
2022-04-26 23:04:48 -0300

    feat(number): add getKmRadius method

--- src/Number.js
+++ src/Number.js
@@ -21,6 +21,37 @@ export class Number {
   }
 
   /**
+   * Get km radius between two coordinates.
+   *
+   * @param {{ latitude: number, longitude: number }} centerCord
+   * @param {{ latitude: number, longitude: number }} pointCord
+   * @return {number}
+   */
+  static getKmRadius(centerCord, pointCord) {
+    const deg2rad = deg => deg * (Math.PI / 180)
+
+    const radius = 6371
+
+    const { latitude: latitude1, longitude: longitude1 } = centerCord
+    const { latitude: latitude2, longitude: longitude2 } = pointCord
+
+    const dLat = deg2rad(latitude2 - latitude1)
+    const dLon = deg2rad(longitude2 - longitude1)
+
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(latitude1)) *
+        Math.cos(deg2rad(latitude2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2)
+
+    const center = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    const distance = radius * center
+
+    return distance
+  }
+
+  /**
    * Get the lower number from an array of numbers.
    *
    * @param {number[]} numbers
--- tests/Unit/NumberTest.js
+++ tests/Unit/NumberTest.js
@@ -20,6 +20,20 @@ describe('\n Numbers Class', () => {
     expect(higher).toBe(5)
   })
 
+  it('should get km radius between two coordinates', () => {
+    const first = Number.getKmRadius(
+      { latitude: -25503207, longitude: -545390592 },
+      { latitude: -25503207, longitude: -545390592 },
+    )
+    const second = Number.getKmRadius(
+      { latitude: -25503207, longitude: -545390592 },
+      { latitude: -254957901, longitude: -545671577 },
+    )
+
+    expect(first).toBe(0)
+    expect(second).toBe(5338.683217695541)
+  })
+
   it('should extract all numbers inside of a string in number and array format', () => {
     const stringWithNumbers =
       "My name is João Lenon and I've 20 years old and I've been living in Foz do Iguaçu for 18 years"
