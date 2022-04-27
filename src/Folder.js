3be88bb0e2eeb6f6ae3ffd3153739e1f0169a7b3
João Lenon <lenonSec7@gmail.com>
2022-04-27 09:11:09 -0300

    fix(tests): replace all \' by '/' in glob pattern

--- src/Folder.js
+++ src/Folder.js
@@ -650,7 +650,7 @@ export class Folder {
     this.loadSync({ withSub: true, isInternalLoad: true })
 
     if (pattern) {
-      pattern = normalize(`${this.path}${sep}${pattern}`)
+      pattern = `${this.path.replace(/\\/g, '/')}/${pattern}`
     }
 
     const files = []
@@ -683,7 +683,7 @@ export class Folder {
     this.loadSync({ withSub: true, isInternalLoad: true })
 
     if (pattern) {
-      pattern = normalize(`${this.path}${sep}${pattern}`)
+      pattern = `${this.path.replace(/\\/g, '/')}/${pattern}`
     }
 
     const folders = []
